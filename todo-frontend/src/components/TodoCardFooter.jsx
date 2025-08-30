86a478a6e3994a50dd66b128a9074c10ea38df55
Nnange <awong.nnange@yahoo.com>
2025-08-30 20:20:27 +0200

    add TodoInput component and integrate it into TodoCard

--- todo-frontend/src/components/TodoCardFooter.jsx
+++ todo-frontend/src/components/TodoCardFooter.jsx
@@ -1,10 +1,10 @@
 function TodoCardFooter() {
   return (
     <div className="border-t grid grid-cols-6">
-      <div  className="col-span-2 p-3 text-sm text-gray-500">
+      <div  className="col-span-2 py-3 text-sm text-gray-500">
         Hello there!
       </div>
-        <div className="col-span-4 p-3 text-sm text-gray-500 text-right">
+        <div className="col-span-4 py-3 text-sm text-gray-500 text-right">
             &copy; 2024 TaskFlow. All rights reserved.
         </div>
     </div>
