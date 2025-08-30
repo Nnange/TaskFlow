58cb11d2e487b8763e6de0c2e98c0f4d1eb91c0d
Nnange <awong.nnange@yahoo.com>
2025-08-30 19:56:00 +0200

    feat: add Header and TodoCardFooter components with react-icons integration

--- todo-frontend/src/components/TodoCard.jsx
+++ todo-frontend/src/components/TodoCard.jsx
@@ -1,12 +1,17 @@
-
+import TodoCardFooter from "./TodoCardFooter";
+import Header from "./Header";
 
 function TodoCard() {
   return (
     <div className="w-full h-screen bg-blue-100 flex items-center justify-center">
-        <div className="border rounded p-4 bg-white shadow-mdt text-black w-1/2">
-        <h2 className="todo-title">Todo Title</h2>
-        <p className="todo-description">This is a description of the todo item.</p>
-        <button className="">Mark as Complete</button>
+        <div className="border rounded-lg  bg-white shadow-mdt text-black w-1/3">
+        <Header />
+        
+        <div className="p-4">
+
+        
+           <TodoCardFooter />
+        </div>
         </div>
     </div>
   );
---
+++ todo-frontend/src/components/TodoCardFooter.jsx
@@ -0,0 +1,14 @@
+function TodoCardFooter() {
+  return (
+    <div className="border-t grid grid-cols-6">
+      <div  className="col-span-2 p-3 text-sm text-gray-500">
+        Hello there!
+      </div>
+        <div className="col-span-4 p-3 text-sm text-gray-500 text-right">
+            &copy; 2024 TaskFlow. All rights reserved.
+        </div>
+    </div>
+  );
+}
+
+export default TodoCardFooter;
