b3e717ca955d53c277ecc55ed1b308ed45197a67
Nnange <awong.nnange@yahoo.com>
2025-09-01 14:28:09 +0200

    feat: implement TodoService for business logic and CRUD operations on Todo entities

---
+++ todo-backend/src/main/java/com/project/todobackend/service/TodoService.java
@@ -0,0 +1,38 @@
+package com.project.todobackend.service;
+
+import com.project.todobackend.entity.Todo;
+import com.project.todobackend.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/*
+Service Layer → keeps business logic separate from controllers.
+
+If later you add things like authentication, validation, or notifications, you put them here.
+
+📌 Role: The logic brain that decides what happens before talking to the database.
+*/
+@Service
+public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo addTodo(Todo todo){
+        return todoRepository.save(todo);
+    }
+    public Todo updateTodo(Long id, Todo updated){
+        updated.setId(id);
+        return todoRepository.save(updated);
+    }
+    public void deleteTodo(Long id){
+        todoRepository.deleteById(id);
+    }
+}
