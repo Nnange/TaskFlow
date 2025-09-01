3d5d2ffd5314b32a207b12e532fee28fa5f39f3d
Nnange <awong.nnange@yahoo.com>
2025-09-01 14:27:35 +0200

    feat: create Todo entity with fields for task and completion status

---
+++ todo-backend/src/main/java/com/project/todobackend/entity/Todo.java
@@ -0,0 +1,41 @@
+package com.project.todobackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String task;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
