40b330a3a8c4c4613523a1d06ea4ed6c18daf756
Nnange <awong.nnange@yahoo.com>
2025-09-04 15:27:55 +0200

    feat: update CORS configuration to allow additional origins for improved accessibility

--- todo-backend/src/main/java/com/project/todobackend/config/CorsConfig.java
+++ todo-backend/src/main/java/com/project/todobackend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allows CORS for all endpoints
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://192.168.2.90:3001", "https://taskflow.nnangedev.duckdns.org/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*"); // Allow all headers
                 // .allowCredentials(true); // Allow cookies/auth if needed
