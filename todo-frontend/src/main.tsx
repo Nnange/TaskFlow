acfe133fa496b68b19967b3a3ae0a404bf6fc1b0
Nnange <awong.nnange@yahoo.com>
2025-09-10 23:54:29 +0200

    renamed approuter

--- todo-frontend/src/main.tsx
+++ todo-frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { AppRouter } from './AppRouter.tsx'
+import { AppRouter } from './Approuter'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 
