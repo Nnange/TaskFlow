39c6d82f812b0bfb68a23f9325456a81c4c7f54d
Nnange <awong.nnange@yahoo.com>
2025-12-13 23:52:32 +0100

    updated the docker compose file

--- todo-backend/src/main/java/com/project/todobackend/config/CorsConfig.java
+++ todo-backend/src/main/java/com/project/todobackend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://192.168.178.36:3001", "https://taskflow.nnangedev.duckdns.org/")); // frontend dev
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://192.168.178.36:3001", "https://todo.awongnnange.com/")); // frontend dev
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
